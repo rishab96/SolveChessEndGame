e61233ef52c1113d928d6a45585b81f444b18c6b
Zack Herbst <zherbst@stanford.edu>
2015-03-09 23:17:27 -0700

    added fire method

--- battleship.c
+++ battleship.c
@@ -10,6 +10,7 @@
 
 void drawField(int nrow, int ncol, int width, int height, int x, int y);
  void add_battleship(int start_row, int start_col, int end_row, int end_col);
+ void fire(int row, int col);
 
 void notmain() {
 	gfx_init();
@@ -20,6 +21,11 @@ void notmain() {
 	drawField(NUM_COLS, NUM_ROWS, TILE_SIZE, TILE_SIZE, START_POS, START_POS);
     add_battleship(3,3,3,7);
     add_battleship(8,8,11,8);
+    fire(1,1);
+    fire(2,2);
+    fire(3,3);
+    fire(5,7);
+    fire(9,9);
 }
 
 void drawField(int nrow, int ncol, int width, int height, int x, int y){
@@ -35,10 +41,10 @@ void drawField(int nrow, int ncol, int width, int height, int x, int y){
 
 //adds a battleship to the field
 void add_battleship(int start_row, int start_col, int end_row, int end_col) {
-    int start_x = START_POS + (start_col * TILE_SIZE) + SHIP_SPACE;
-    int start_y = START_POS + (start_row *TILE_SIZE) + SHIP_SPACE;
-    int end_x = 0;
-    int end_y = 0; 
+    volatile int start_x = START_POS + (start_col * TILE_SIZE) + SHIP_SPACE;
+    volatile int start_y = START_POS + (start_row *TILE_SIZE) + SHIP_SPACE;
+    volatile int end_x = 0;
+    volatile int end_y = 0; 
     //determine if it is placed horizontally or vertically
     if(start_row == end_row) {
         end_x = start_x + (end_col-start_col) * TILE_SIZE - 2*SHIP_SPACE;
@@ -52,4 +58,22 @@ void add_battleship(int start_row, int start_col, int end_row, int end_col) {
     int width = end_x - start_x;
     int height = end_y - start_y;
     gfx_draw_rect(FIELD_COLOR, start_x, start_y, width, height);
+}
+
+void fire(int row, int col) {
+    //draw a red X on the square
+    volatile int start_x = START_POS + (col * TILE_SIZE);
+    volatile int start_y = START_POS + (row *TILE_SIZE);
+    int red = gfx_compute_color(255,0,0);
+
+    //draw first half
+    volatile int end_x = start_x + TILE_SIZE;
+    volatile int end_y = start_y + TILE_SIZE;
+    
+    gfx_draw_line(red, start_x, end_x, start_y, end_y, 0);
+
+    //draw second half of X
+    volatile int new_start_y = start_y + TILE_SIZE;
+    volatile int new_end_y = end_y - TILE_SIZE;
+    gfx_draw_line(red, start_x, end_x, new_start_y, new_end_y, 0);
 }
